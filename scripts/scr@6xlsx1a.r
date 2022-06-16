3c37eb78abcf83bb0b2d3fec424d03993cdb4058
Kilem L. Gwet <105383492+gwet237@users.noreply.github.com>
2022-06-16 14:55:58 -0400

    Script scr@6xlsx1a.r modified to add solid borders to the summary table

--- scripts/scr@6xlsx1a.r
+++ scripts/scr@6xlsx1a.r
@@ -29,9 +29,8 @@ write.xlsx(x = as.data.frame(summary.df),
            sheetName = "summaryStats1",append = TRUE,
            row.names = FALSE)
 
-#- Write the summary.df to a specific location in a different
-#  worksheet named "summaryStats2" belonging to the same  
-#  workbook chap6datasets1a.xlsx.
+#- Write summary.df to a different worksheet named "summaryStats2".
+#  Format top and bottom rows and add solid borders to the table.
 wb.df2 <- loadWorkbook(file="./data/chap6datasets1a.xlsx")
 my.sheet <- createSheet(wb=wb.df2, sheetName="summaryStats2")
 
@@ -39,6 +38,8 @@ MaxRow <- summary.df %>%
   summarise(across(c(Group,Target),~"All(Max)"),
             across(!c(Group,Target),max))
 
+# Create 3 cell blocks for the 3 parts of the table (top row +
+# bottom row and main table with data values)
 topRow.cb <- CellBlock(sheet=my.sheet, startRow=5, startColumn=3,
                        noRows = 1, noColumns = 10)
 botRow.cb <- CellBlock(sheet=my.sheet, startRow=11, startColumn=3,
@@ -46,12 +47,21 @@ botRow.cb <- CellBlock(sheet=my.sheet, startRow=11, startColumn=3,
 data.cb <- CellBlock(sheet=my.sheet, startRow=6, startColumn=3,
                      noRows = 5, noColumns = 10)
 
+# Define the cell styles that we need.
+borders.sty <- Border(color="black", 
+                      position=c("BOTTOM", "LEFT","TOP","RIGHT"), 
+                      pen=c("BORDER_THIN", "BORDER_THIN",
+                            "BORDER_THIN","BORDER_THIN"))
+
 topRow.cs <- CellStyle(wb.df2) + 
   Alignment(horizontal="ALIGN_CENTER") +
-  Font(wb.df2,isBold=F,isItalic=F,color="yellow",heightInPoints=12)
+  Font(wb.df2,isBold=F,isItalic=F,color="yellow",heightInPoints=12)+
+  borders.sty
 botRow.cs <- CellStyle(wb.df2) + 
   Alignment(horizontal="ALIGN_CENTER") +
-  Font(wb.df2, isBold=T,isItalic=F,color="9")
+  Font(wb.df2, isBold=T,isItalic=F,color="9") + borders.sty
+borders.cs <- CellStyle(wb.df2) + borders.sty
+
 top.fill <- Fill(foregroundColor = "blue", 
                  backgroundColor="blue")
 bot.fill <- Fill(foregroundColor = "darkred", 
@@ -63,7 +73,7 @@ CB.setRowData(cellBlock = botRow.cb,x=MaxRow,
               rowIndex=1,rowStyle = botRow.cs)
 CB.setMatrixData(cellBlock = data.cb,
                  x=as.matrix(summary.df),
-                 startRow=1,startColumn = 1)
+                 startRow=1,startColumn = 1,cellStyle=borders.cs )
 
 CB.setFill(topRow.cb, fill=top.fill, rowIndex=rep(1,10),
            colIndex=1:10)
